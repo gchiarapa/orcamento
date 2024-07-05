705025e7fa318fef6f87b27cd95e199fad572ad4
gchiarapa <50337428+gchiarapa@users.noreply.github.com>
2024-07-05 18:19:51 +0000

    forms orcamento

---
+++ src/app/modules/orcamento/model/orcamento-metricis-values.ts
@@ -0,0 +1,5 @@
+export interface OrcamentoMetricsValues {
+    
+    aluminiumValue : string;
+    workValue: string;
+}
---
+++ src/app/modules/orcamento/model/orcamento-value.ts
@@ -0,0 +1,14 @@
+export interface OrcamentoValue {
+
+    size: string,
+    freight: string,
+    total: string,
+    name: string,
+    email: string,
+    phone: number,
+    street: string,
+    postalCode: string,
+    state: string,
+    district: string,
+
+}
